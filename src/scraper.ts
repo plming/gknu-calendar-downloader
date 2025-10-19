ce86920e2a9f6fdfdd852389bc5c94c3b7152e28
김현석 <kim010322@gmail.com>
2025-10-19 22:34:02 +0900

    Revert "change into allorigins"

--- src/scraper.ts
+++ src/scraper.ts
@@ -40,7 +40,7 @@ async function fetchEvents(
   }
 
   const BASE_URL = "https://www.gknu.ac.kr";
-  const CORS_PROXY = "https://api.allorigins.win/raw?url=";
+  const CORS_PROXY = "https://corsproxy.io/?";
 
   const gknuUrl = `${BASE_URL}/main/module/schedule/view.do?category1=101&notice_start_date=${year}-${month
     .toString()
