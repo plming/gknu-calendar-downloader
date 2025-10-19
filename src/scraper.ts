1eca9185e26559ed3bb222d476783b990c64dae7
Hyeonseok Kim <kim010322@gmail.com>
2025-10-19 18:21:28 +0900

    change cors proxy

--- src/scraper.ts
+++ src/scraper.ts
@@ -40,7 +40,7 @@ async function fetchEvents(
   }
 
   const BASE_URL = "https://www.gknu.ac.kr";
-  const CORS_PROXY = "https://corsproxy.io/?";
+  const CORS_PROXY = "https://api.allorigins.win/get?url=";
 
   const gknuUrl = `${BASE_URL}/main/module/schedule/view.do?category1=101&notice_start_date=${year}-${month
     .toString()
@@ -56,11 +56,12 @@ async function fetchEvents(
   }
 
   const data = await response.json();
-  if (!validate(data)) {
+  const parsed = JSON.parse(data.contents);
+  if (!validate(parsed)) {
     throw new Error("Invalid data format received from GKNU");
   }
 
-  return data;
+  return parsed;
 }
 
 function validate(events: unknown): events is GknuCalendarEvent[] {
